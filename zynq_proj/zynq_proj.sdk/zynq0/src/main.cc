99bf72bd7d3986e5030193eefb08152ed7185a38
CHONG <hai-hsiong-ivan_chong@keysight.com>
2018-04-17 15:48:40 +0800

    add sdk files

---
+++ zynq_proj/zynq_proj.sdk/zynq0/src/main.cc
@@ -0,0 +1,20 @@
+/*
+ * Empty C++ Application
+ */
+
+#include <iostream>
+
+#include "testperiph.h"
+using namespace std;
+
+int main()
+{
+	//init_platform();
+	//print("Hello World\n\r");
+
+	cout << "Hello" << endl;
+	cout << "World" << endl;
+
+	testperiph();
+	return 0;
+}
